5f76946a86ffe512431ab82f682b61b70f139de9
kylymbai <kylymbaialmazbekuulu@gmail.com>
2022-11-26 00:32:42 +0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,70 +1,13 @@
-class Cars {
-    constructor(options) {
-    this.model = options.model
-    this.color = options.color
-    this.wheels = options.wheels
-    }
+let num = prompt("Оцените наш сервис от 1 до 10");
 
-    start() {
-        console.log('Машина заведена!')
-    }
+if(num < 1){
+    alert("оценка должна быть от 1 до 10")
+}else if(num > 10){
+    alert("оценка должна быть от 1 до 10")
+}else if(num <= 5){
+    alert("мы работаем над улучшением нашего сервиса")
+}else if(num <= 10){
+    alert("Спасибо за высокую оценку!")
+}else{
+    alert("ERROR! Write a number!")
 }
-
-const car = new Cars ({
-    model: 'BMW',
-    color: 'black',
-    wheels: 4
-})
-console.log(car)
-car.start()
-
-class Mers extends Cars {
- constructor(options){
-    super(options)
-    this.capasity = options.capasity
- }
- 
-}
-
-const mers = new Mers({
-    model: 'mers',
-    color: 'white',
-    wheels: 4,
-    capasity: 5
-})
-console.log(mers)
-
-class Kia extends Cars {
-    constructor(options) {
-        super(options)
-        this.wheel = options.wheel
-    }
-}
-
-const kia = new Kia ({
-    mers: 'kia',
-    color: 'red',
-    capasity: '3',
-    wheels: 4,
-    wheel: 'left'
-})
-
-console.log(kia)
-
-class Toyota extends Cars {
-    constructor(options){
-        super(options)
-        this.body = options.body
-    }
-}
-
-const toyota = new Toyota ({
-    model: 'toyota',
-    color: 'green',
-    wheels: 4,
-    capasity: 2,
-    wheel: 'right',
-    body: 'sedan'
-})
-
-console.log(toyota)
